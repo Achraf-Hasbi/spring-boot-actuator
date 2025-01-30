043de8dca2fe70a83300ff062b94d22b8ccfa234
Achraf <achraf.hasbi@gmail.com>
2025-01-30 23:52:18 +0100

    Use @Timed

--- src/main/java/com/medium/actuator/controller/GreetingController.java
+++ src/main/java/com/medium/actuator/controller/GreetingController.java
@@ -1,7 +1,6 @@
 package com.medium.actuator.controller;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class GreetingController {
 
-    private final MeterRegistry meterRegistry;
-
     @GetMapping("/greeting")
+    @Timed(value = "my.greeting", extraTags = {"env", "local"})
     public String greeting() throws InterruptedException {
-        Timer.Sample sample = Timer.start(meterRegistry);
         // Sleep between 0s and 5s
         Thread.sleep((long) (Math.random() * 5000));
 
-        sample.stop(meterRegistry.timer("request.greeting", "env", "local"));
         return "Hello From Spring Boot App !";
     }
 }
